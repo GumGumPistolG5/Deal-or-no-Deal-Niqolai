83922838e22c6c65614d6dc1360bf38b0cb27e5c
utannj1 <utannj1@D201-71860>
2025-11-04 13:29:26 +1300

    .

---
+++ Deal-Or-No-Deal_Niqolai-up/Deal-Or-No-Deal_Niqolai-up/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Deal_Or_No_Deal_Niqolai_up
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
